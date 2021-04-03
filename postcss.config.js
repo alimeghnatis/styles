bff7eaac48d4cc69a5e182db94235ec033f5ca47
779g <adrian.villa@779.mx>
2021-04-03 20:40:54 +0000

    First commit

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    'css-mqpacker':{},
+    'postcss-preset-env':
+    {
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
+    },
+    'cssnano': {
+      'preset':[
+        'default',
+        { "discardComments": { "removeAll":true }}
+      ]
+    }
+  }
+}
+
+
+
+
+
